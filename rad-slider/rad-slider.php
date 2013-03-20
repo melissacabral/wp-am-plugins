39e7dee4e9c8e9127fc0e771b8762392cae05bb2
melissacabral <melissacabral@gmail.com>
2013-03-19 21:26:29 -0700

    Update rad-slider.php

--- rad-slider/rad-slider.php
+++ rad-slider/rad-slider.php
@@ -9,7 +9,10 @@ License: GPLv2
 Author URI: http://melissacabral.com
 */
 
-/* Register a Custom Post Type (Slide) */
+/**
+ * Register a Custom Post Type (Slide) 
+ * Since ver. 1.0
+ */
 add_action('init', 'slide_init');
 function slide_init() {
 	$args = array(
@@ -28,21 +31,21 @@ function slide_init() {
 			'menu_name' => 'Slides'
 		),
 		'public' => true,
-		'publicly_queryable' => true,
-		'show_ui' => true, 
+		'exclude_from_search' => true
 		'show_in_menu' => true, 
-		'query_var' => true,
 		'rewrite' => true,
-		'capability_type' => 'post',
 		'has_archive' => true, 
 		'hierarchical' => false,
-		'menu_position' => null,
+		'menu_position' => 20,
 		'supports' => array('title', 'editor', 'thumbnail')
 	); 
 	register_post_type('slide', $args);
 }
 
-/* Put  little help box at the top of the editor */
+/**
+ * Put  little help box at the top of the editor 
+ * Since ver 1.0
+ */
 add_action('contextual_help', 'slide_help_text', 10, 3);
 function slide_help_text($contextual_help, $screen_id, $screen) {
 	if ('slide' == $screen->id) {
@@ -60,75 +63,22 @@ function slide_help_text($contextual_help, $screen_id, $screen) {
 }
 
 
-//This prevents 404 errors when viewing our custom post archives
-//always do this whenever introducing a new post type or taxonomy
+/**
+ * This prevents 404 errors when viewing our custom post archives
+ * always do this whenever introducing a new post type or taxonomy
+ * Since ver 1.0
+ */
 function rad_slider_rewrite_flush(){
 	slide_init();
 	flush_rewrite_rules();
 }
-//run this when the plugin activates
 register_activation_hook(__FILE__, 'rad_slider_rewrite_flush');
 
-/* Add the image size for the slider */
+/**
+* Add the image size for the slider 
+* Since ver 1.0
+*/
 function rad_slider_image(){
 	add_image_size( 'rad-slider', 960, 330, true );	
 }
 add_action('init', 'rad_slider_image');
-
-//Front-end display. Call this function in your theme wherever you want your slider
-function rad_slider(){ 
-	//set up custom loop
-	$slide_query = new WP_Query( array(
-		'post_type' => 'slide',
-		'posts_per_page' => 5
-	) );
-	if( $slide_query->have_posts() ):
-?>
-	<div id="rad-slider">
-    	<ul class="slides">
-        <?php while( $slide_query->have_posts() ):
-			$slide_query->the_post();  ?>
-        	<li>
-            	<?php the_post_thumbnail('rad-slider'); ?>
-                <div class="slide-details">
-                	<h2><?php the_title(); ?></h2>
-                    <p><?php the_content(); ?></p>
-                </div>
-            </li>
-        <?php endwhile; ?>
-        </ul>
-        <a href="javascript:;" id="previous-slide">Previous</a>
-        <a href="javascript:;" id="next-slide">Next</a>
-        
-    </div>	
-<?php
-	endif; // have_posts. end of custom loop.
-	wp_reset_postdata(); //resume normal 'page' behavior
- }
- 
-//Load up all JS and CSS
-function rad_slider_scripts(){
-	//get the WP version of jQuery
-	wp_enqueue_script('jquery');
-	
-	//tell WP about the cycle plugin
-	$cycle_path = plugins_url('js/jquery.cycle.all.js', __FILE__);
-	wp_register_script('jq-cycle', $cycle_path);
-	wp_enqueue_script('jq-cycle');
-	
-	//tell WP about our custom.js file
-	$custom_path = plugins_url('js/custom.js', __FILE__);
-	wp_register_script('cycle-custom', $custom_path);
-	wp_enqueue_script('cycle-custom');	
-	
-	//tell WP about the stylesheet
-	$style_path = plugins_url('css/style.css', __FILE__);
-	wp_register_style('cycle-style', $style_path);
-	wp_enqueue_style('cycle-style');
-}
-add_action('wp_enqueue_scripts', 'rad_slider_scripts');
-
-
-
-
-
